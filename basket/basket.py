2a18db68c2d182c831997ea94578e3f8fdcf00f4
Manuel Ferrero <pety2do@hotmail.com>
2022-10-18 20:07:24 -0300

    Update 5.2022.10.18

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,4 @@
-
+from store.models import Product
 
 
 class Basket():
@@ -16,13 +16,69 @@ class Basket():
         self.basket = basket
 
 
-    def add(self, product):
+    def add(self, product, qty):
         """
-        Agregar y actualizar la información de la sesion del carrito del usuario
+        Agrega y actualiza la información de la sesión del carrito del usuario
         """
-        product_id = product.id
+        product_id = str(product.id)
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': float(product.price)}
-        
+            self.basket[product_id] = {'price': float(product.price), 'qty': int(qty)}
+        else:
+            self.basket[product_id]['qty'] += int(qty)
+        self.save_session()
+    
+
+    def delete(self, product_id):
+        """
+        Borra y actualiza la información de la sesión del carrito
+        """
+        product_id = str(product_id)
+        if product_id in self.basket:
+            del self.basket[product_id]
+        self.save_session()
+    
+    
+    def update(self, product_id, product_qty):
+        """
+        Actualiza la cantidad de un producto en de la sesión del carrito
+        """
+        product_id = str(product_id)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = product_qty
+        self.save_session()
+
+
+    def save_session(self):
         self.session.modified = True
+
+
+    def get_total_price(self):
+        """
+        Obtiene los totales de todos los productos y devuelve el total
+        """
+        return float(sum(item['price'] * item['qty'] for item in self.basket.values()))
+    
+
+    def __iter__(self):
+        """
+        Recoge los product_id en la información de la sesión para hacer un query en la base de datos
+        y retornar los productos como iterable
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['total_price'] = float(item['price'] * item['qty'])
+            yield item  
+
+
+    def __len__(self):
+        """
+        Obtiene la información del carrito y cuenta la cantidad de items
+        """
+        return sum(item['qty'] for item in self.basket.values()) 
--- basket/views.py
+++ basket/views.py
@@ -13,7 +13,32 @@ def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
-        response = JsonResponse({'test': 'data'})
+        basket.add(product=product, qty=product_qty)
+        basket_qty = len(basket)
+        response = JsonResponse({'qty': basket_qty})
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product_id=product_id)
+        basket_qty = basket.__len__()
+        basket_total = basket.get_total_price()
+        response = JsonResponse({'total': basket_total})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product_id=product_id, product_qty=product_qty)
+        basket_qty = basket.__len__()
+        basket_total = basket.get_total_price()
+        response = JsonResponse({'qty': basket_qty, 'total': basket_total})
         return response
