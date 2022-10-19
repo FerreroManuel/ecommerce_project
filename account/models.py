33b74bc8b8feb6c24920ed28f460efde25fa963b
Manuel Ferrero <pety2do@hotmail.com>
2022-10-18 23:51:34 -0300

    Account app created

---
+++ account/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+
+class CustomAccountManager(BaseUserManager):
+    
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        # Validations
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.'
+            )
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.'
+            )
+
+        return self.create_user(email, user_name, password, **other_fields)
+    
+
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    # Personal information
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+    # Contact information
+    country = CountryField(blank=True)
+    state = models.CharField(max_length=150, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    cell_phone = PhoneNumberField(blank=True)
+    home_phone = PhoneNumberField(blank=True)
+
+    # User status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Configuration data
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'user_name',
+        'first_name',
+        'last_name',
+        'country',
+        'state',
+        'city',
+        'postcode',
+        'address_line_1',
+        'address_line_2',
+        'cell_phone',
+    ]
+
+
+    class Meta:
+        verbose_name = 'Accounts'
+        verbose_name_plural = 'Accounts'
+    
+
+    def __str__(self):
+        return self.user_name
--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media folder
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = 'account/dashboard'
+LOGIN_URL = 'account/login/'
